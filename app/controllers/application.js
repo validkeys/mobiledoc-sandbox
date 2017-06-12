c4b6e80619633502defecbd66ffaf96a0c7d2a86
Kyle Davis <validkeys@gmail.com>
2017-06-12 12:55:54 -0400

    init

---
+++ app/controllers/application.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    mobiledocWasUpdated(updatedMobiledoc) {
+      Ember.Logger.log('New mobiledoc:',updatedMobiledoc);
+      // note that this action will be fired for every changed character,
+      // so you may want to debounce API calls if you are using it for
+      // an "autosave" feature.
+    },
+    willCreateEditor() {
+      Ember.Logger.log('about to create the editor');
+    },
+    didCreateEditor(editor) {
+      Ember.Logger.log('created the editor:', editor);
+
+      editor.registerKeyCommand({
+        str: 'enter',
+        run(editor) {
+          console.log('enter key hit');
+        }
+      });
+
+      editor.registerKeyCommand({
+        str: 'META+enter',
+        run(editor) {
+          console.log('META+enter key hit');
+        }
+      });
+
+      editor.onTextInput({
+        text: '@',
+        run(editor) {
+          console.log("@ sign typed", this.controller);
+        }
+      })
+
+    }
+  }
+});
